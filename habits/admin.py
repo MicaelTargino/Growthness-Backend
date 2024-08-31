26b4602fda78c05220fb1fcdc97e5ee8ebabe73c
MicaelTargino <micaelgomestargino@gmail.com>
2024-08-31 10:59:59 -0300

    Add models to admin

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Frequency, Habit, HabitLog
 
-# Register your models here.
+@admin.register(Frequency)
+class FrequencyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+    filter_horizontal = ('frequencies',) 
+
+@admin.register(HabitLog)
+class HabitLogAdmin(admin.ModelAdmin):
+    list_display = ('habit', 'amount', 'date')
