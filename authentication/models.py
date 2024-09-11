de943b2460b793ad2cfb87e0b6002973b25990be
MicaelTargino <micaelgomestargino@gmail.com>
2024-09-11 18:08:52 -0300

    Add more customizable data for user Model

--- authentication/models.py
+++ authentication/models.py
@@ -2,9 +2,22 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .manager import CustomUserManager
 # Create your models here.
+
+class UserGoals(models.Model):
+    title = models.CharField(max_length=255)
+
 class User(AbstractUser):
     username = None  # Remove the default username field
     email = models.EmailField(unique=True)  # Make email unique
+    
+    weight = models.FloatField(null=True, blank=True)
+    weight_measure = models.CharField(max_length=20, default="kg")
+
+    height = models.FloatField(null=True, blank=True)
+    height_measure = models.CharField(max_length=20, default="m")
+
+    goals = models.ManyToManyField(UserGoals, blank=True, null=True, related_name="user_goals")
+
 
     USERNAME_FIELD = 'email'  # Set email as the unique identifier for authentication
     REQUIRED_FIELDS = ['password']  # Remove username from required fields
